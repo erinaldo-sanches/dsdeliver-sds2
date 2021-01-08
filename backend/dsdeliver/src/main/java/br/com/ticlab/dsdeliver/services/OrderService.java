77bfdb239e6fa5aaad55bc735dca2ec94fbdfbef
Erinaldo Sanches Nascimento Erinaldo <erinaldo.sanches@ticlab.com.br>
2021-01-08 17:26:38 -0300

    POST /orders

--- backend/dsdeliver/src/main/java/br/com/ticlab/dsdeliver/services/OrderService.java
+++ backend/dsdeliver/src/main/java/br/com/ticlab/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package br.com.ticlab.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.ticlab.dsdeliver.dto.OrderDTO;
+import br.com.ticlab.dsdeliver.dto.ProductDTO;
 import br.com.ticlab.dsdeliver.entities.Order;
+import br.com.ticlab.dsdeliver.entities.OrderStatus;
+import br.com.ticlab.dsdeliver.entities.Product;
 import br.com.ticlab.dsdeliver.respositories.OrderRepository;
+import br.com.ticlab.dsdeliver.respositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,11 +22,27 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findOrderWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 		
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 
 }
