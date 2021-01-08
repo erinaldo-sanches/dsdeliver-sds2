97ceb56b69e449922337f2e9e8604949473ad6c9
Erinaldo Sanches Nascimento Erinaldo <erinaldo.sanches@ticlab.com.br>
2021-01-08 15:31:48 -0300

    GET /orders

---
+++ backend/dsdeliver/src/main/java/br/com/ticlab/dsdeliver/services/OrderService.java
@@ -0,0 +1,27 @@
+package br.com.ticlab.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.ticlab.dsdeliver.dto.OrderDTO;
+import br.com.ticlab.dsdeliver.entities.Order;
+import br.com.ticlab.dsdeliver.respositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrderWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+		
+	}
+
+}
