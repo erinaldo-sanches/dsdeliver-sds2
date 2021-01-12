e1e2e0b44d20529238f74e20fb62185c94c53736
Erinaldo Sanches Nascimento Erinaldo <erinaldo.sanches@ticlab.com.br>
2021-01-12 11:33:22 -0300

    Create front-web initials files

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,16 +1,11 @@
 import { useEffect } from 'react';
 import './App.css';
-import Counter from './Counter';
 
 function App() {
 
-  useEffect(() => {
-    console.log('Componente iniciou!');
-  },[]);
-
   return (
     <div>
-      <Counter />
+      
     </div>
     
   );
