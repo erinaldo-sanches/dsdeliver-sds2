afd14458cea9727bc5914c2fbbe07ac41c551159
Erinaldo Sanches Nascimento Erinaldo <erinaldo.sanches@ticlab.com.br>
2021-01-13 16:28:46 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,12 +2,14 @@ import { useEffect } from 'react';
 import './App.css';
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
 
   return (
     <div>
       <Navbar/>
+      <Home />
     </div>
     
   );
